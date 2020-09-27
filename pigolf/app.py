1e2cb3518eacda493c6fb7d100620cc7e1af2bf8
ijpatter1 <ian@tunameltsmyheart.com>
2020-09-26 20:12:56 -0400

    set parent frame bg to black

--- pigolf/app.py
+++ pigolf/app.py
@@ -94,6 +94,7 @@ class App(tk.Frame):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         # define our parent frame config
         self.parent = parent
+        self.parent.configure(background="black")
         self.parent.title("PIGOLF")
         self.parent.minsize(480, 800)
         self.parent.maxsize(480, 800)
