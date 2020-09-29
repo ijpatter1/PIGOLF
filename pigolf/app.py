4cd823f02865a7a23a352f509af79d817927bb8c
ijpatter1 <ian@tunameltsmyheart.com>
2020-09-29 12:20:11 -0400

    idiot

--- pigolf/app.py
+++ pigolf/app.py
@@ -94,7 +94,7 @@ class TabBar():
                                      background="black", highlightbackground="black",
                                      activebackground="black", selectcolor="black")
         self.recBtn.image_ref = (self.recImg, self.stpImg)
-        self.recBtn.grid(row=1, column=1, sticky=tk.N)
+        self.recBtn.grid(row=1, column=1, sticky=tk.N, pady=(0, 15))
 
 
 class App(tk.Frame):
