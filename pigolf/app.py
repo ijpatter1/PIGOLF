7e16382a76a64f631d1683991ef26ef7a68fbacd
ijpatter1 <ian@tunameltsmyheart.com>
2020-09-29 12:29:08 -0400

    change btn pady to (5, 0)

--- pigolf/app.py
+++ pigolf/app.py
@@ -94,7 +94,7 @@ class TabBar():
                                      background="black", highlightbackground="black",
                                      activebackground="black", selectcolor="black")
         self.recBtn.image_ref = (self.recImg, self.stpImg)
-        self.recBtn.grid(row=1, column=1, pady=(10, 0))
+        self.recBtn.grid(row=1, column=1, pady=(5, 0))
 
 
 class App(tk.Frame):
