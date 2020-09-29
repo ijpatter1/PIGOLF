9bc782e1b2146e805c5b79df9c4063c265a8f5ba
ijpatter1 <ian@tunameltsmyheart.com>
2020-09-29 10:49:20 -0400

    remove border

--- pigolf/app.py
+++ pigolf/app.py
@@ -88,7 +88,7 @@ class TabBar(tk.Frame):
         self.recBtn = tk.Checkbutton(self.window, image=self.recImg,
                                      selectimage=self.stpImg, indicatoron=0,
                                      variable=self.var, anchor=tk.CENTER,
-                                     background="black")
+                                     background="black", borderwidth=0)
         self.recBtn.image_ref = (self.recImg, self.stpImg)
         self.recBtn.pack()
 
