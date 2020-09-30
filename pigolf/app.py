123cc3b41efd88a21b0a1e8a819bdb4b814e553e
ijpatter1 <ian@tunameltsmyheart.com>
2020-09-29 22:54:59 -0400

    add config window

--- pigolf/app.py
+++ pigolf/app.py
@@ -122,7 +122,8 @@ class App(tk.Frame):
         # define our parent frame config
         self.parent = parent
         self.parent.configure(background="gray", borderwidth=0)
-        self.parent.geometry("476x746+0+0")
+        self.parent.geometry("476x740+0+0")
+        # self.parent.attributes('-zoomed', True)
         self.parent.title("PIGOLF")
 
         # This protocol method is a tkinter built-in method to catch if
