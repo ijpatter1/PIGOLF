b4bb098da65e699fc48bef42eefb9d700607752f
ijpatter1 <ian@tunameltsmyheart.com>
2020-11-03 21:32:18 +0000

    testing 10fps

--- pigolf/app.py
+++ pigolf/app.py
@@ -10,8 +10,8 @@ import picamera.array as array
 
 
 class MySteamingOutput(array.PiRGBAnalysis):
-    def __init__(self, parent, camera, size):
-        super(MySteamingOutput, self).__init__(camera, size)
+    def __init__(self, parent, camera):
+        super(MySteamingOutput, self).__init__(camera)
         self.frame = None
         self.parent = parent
 
@@ -41,7 +41,7 @@ class Camera:
         self.dispArray = None   # array.PiRGBArray(self.camera, size=(self.width, self.height))
         self.delayArray = array.PiRGBArray(self.camera, size=(self.width, self.height))
 
-        self.stream = MySteamingOutput(self, self.camera, 30000000)  # picamera.PiCameraCircularIO(self.camera, seconds=1)
+        self.stream = MySteamingOutput(self, self.camera)  # picamera.PiCameraCircularIO(self.camera, seconds=1)
 
         self.camera.start_recording(self.stream, format='rgb')
 
