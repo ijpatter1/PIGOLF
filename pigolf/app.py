c0be1361cd1749996922070bba5aa805826aca1c
ijpatter1 <ian@tunameltsmyheart.com>
2020-11-04 13:44:26 +0000

    testing: recording at 1024x768, 38ms refresh with 5 sec sleep

--- pigolf/app.py
+++ pigolf/app.py
@@ -106,7 +106,7 @@ class App(tk.Frame):
         self.height = 720
         self.resolution = "1024x768"
         self.framerate = 40
-        self.refresh = 38   # int(1000/self.framerate)
+        self.refresh = 30   # int(1000/self.framerate)
 
         self.queue = queue.Queue()
 
